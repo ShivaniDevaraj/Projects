fbd9d1d53cc052c3c913235fcf4b34c8ea61a1f6
shivani <shivanidevaraj24@gmail.com>
2020-07-10 14:12:46 +0530

    Commit

---
+++ AsyncTaskTest/app/src/main/java/com/example/asynctasktest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.asynctasktest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et = findViewById(R.id.userinput);
+        recyclerView = findViewById(R.id.recycler);
+    }
+
+    public void search(View view) {
+        String bookname = et.getText().toString();
+        MyTask task = new MyTask(this, bookname, recyclerView);
+        task.execute();
+
+    }
+}
